fd0e51cf9b752e6b6141a358b342b5d60065db75
Ayodeji  Emmanuel  Adetayo <60976363+dejidev@users.noreply.github.com>
2025-04-04 11:10:43 +0100

    Update .env.js

--- .env.js
+++ .env.js
@@ -1,1 +1,8 @@
+✅ Generated new key pair 'my-key'
+🔑 Public key: ScgSU/zCFy0duuTJ9aQY2s6NeiUYhpkjfhs9d9SAdg0=
 
+
+
+🔑 Mnemonic for key pair 'my-key':
+
+timber champion fabric fall spot biology very surprise donor whisper remove relief merry lesson smoke carbon nut panda tail dynamic demise cheese fatigue version
